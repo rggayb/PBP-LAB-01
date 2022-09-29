b897c57fc33eb1e35b019ebcc9c6d2129166c805
ranggabrata <ranggabbrata@gmail.com>
2022-09-29 15:19:58 +0700

    Lab 4 done!

--- wishlist/views.py
+++ wishlist/views.py
@@ -19,7 +19,7 @@ def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
     'list_barang': data_barang_wishlist,
-    'nama': 'Rangga Ganteng Parah',
+    'nama': 'Rangga Yudhistira Brata',
     'last_login': request.COOKIES['last_login'],
     }
     return render(request, "wishlist.html", context)
